3e2b06789d41f2e9d4403bed3f42bdfd8821a906
ryoichi <ryoichi.satoyama@gmail.com>
2023-09-08 22:37:50 +0900

    change lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="jp">
       <body className={inter.className}>
         <AuthProvider>
           <div>
