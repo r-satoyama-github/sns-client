004e238d2ba1f10dcc341b149854db12ba86c08d
ryoichi <ryoichi.satoyama@gmail.com>
2023-09-13 23:01:22 +0900

    add humberger menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,22 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "../context/auth";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const handleMenuOpen = () => {
+    setOpen(!isOpen);
+  };
+
+  const handleMenuClose = () => {
+    setOpen(false);
+  };
+  const handleMenuCloseLogout = () => {
+    setOpen(false);
+    logout();
+  };
 
   return (
     <header className="bg-gray-700 p-4 text-white">
@@ -12,19 +24,32 @@ const Navbar = () => {
         <h1 className="font-semibold text-xl">
           <Link href="/">SNS Clone</Link>
         </h1>
-        <nav>
-          <ul className="flex space-x-4">
+        <nav
+          className={
+            isOpen
+              ? "z-40 bg-gray-200 fixed top-0 right-0 bottom-0 left-0 h-screen flex flex-col"
+              : "fixed right-[-100%] md:right-4"
+          }
+        >
+          <ul
+            className={
+              isOpen
+                ? "flex h-screen justify-center items-center flex-col gap-6 text-xl"
+                : "block md:flex md:gap-8"
+            }
+          >
             {user ? (
               <>
                 <Link
                   href={`/profile/${user.id}`}
                   className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+                  onClick={handleMenuClose}
                 >
                   プロフィール
                 </Link>
                 <Link
                   href="/"
-                  onClick={logout}
+                  onClick={handleMenuCloseLogout}
                   className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
                 >
                   ログアウト
@@ -34,12 +59,14 @@ const Navbar = () => {
               <>
                 <Link
                   href="/login"
+                  onClick={handleMenuClose}
                   className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
                 >
                   ログイン
                 </Link>
                 <Link
                   href="/signup"
+                  onClick={handleMenuClose}
                   className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
                 >
                   サインアップ
@@ -48,6 +75,29 @@ const Navbar = () => {
             )}
           </ul>
         </nav>
+        <button className="z-50 space-y-2 md:hidden" onClick={handleMenuOpen}>
+          <span
+            className={
+              isOpen
+                ? "block w-8 h-0.5 bg-gray-600 translate-y-2.5 rotate-45 duration-300"
+                : "block w-8 h-0.5 bg-gray-100 duration-300"
+            }
+          />
+          <span
+            className={
+              isOpen
+                ? "block opacity-0 duration-300"
+                : "block w-8 h-0.5 bg-gray-100 duration-300"
+            }
+          />
+          <span
+            className={
+              isOpen
+                ? "block w-8 h-0.5 bg-gray-600 -rotate-45 duration-300"
+                : "block w-8 h-0.5 bg-gray-100 duration-300"
+            }
+          />
+        </button>
       </div>
     </header>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import { AuthProvider } from "./context/auth";
+// import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="jp">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <div>
