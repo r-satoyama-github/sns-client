455f5d7281a0b84c8e8d3216aa2d675235436993
ryoichi <ryoichi.satoyama@gmail.com>
2023-09-14 23:14:19 +0900

    change text size

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -33,20 +33,20 @@ const Profile = (props: Props) => {
 
   return (
     <div className="bg-white shadow-md rounded-lg p-6 mb-4">
-      <div className="flex items-center">
+      <div className="flex items-center w-full">
         <img
           className="w-20 h-20 rounded-full mr-4"
           alt="User Avatar"
           src={profile.profileImageUrl}
         />
-        <div className="container w-full">
+        <div className="w-full">
           <h2 className="text-2xl font-semibold mb-1">
             {profile.user.username}
           </h2>
           {isEdit ? (
             <div className="container flex w-full justify-between items-center">
               <input
-                className="border-gray-300 bg-gray-100 px-2 py-1 rounded text-gray-600 focus:border-blue-200 focus:ring-blue-200"
+                className="border-gray-300 bg-gray-100 px-2 py-1 rounded text-gray-600 w-6/12"
                 type="text"
                 value={bio}
                 onChange={(e) => setBio(e.target.value)}
@@ -62,11 +62,13 @@ const Profile = (props: Props) => {
             </div>
           ) : (
             <div className="container flex w-full justify-between items-center">
-              <p className="text-gray-600 px-2 py-1">{bio}</p>
+              <p className="text-gray-600 px-2 py-1 text-xs md:text-sm">
+                {bio}
+              </p>
               <button
                 type="button"
                 onClick={(e) => handleEditButton(e)}
-                className="bg-gray-700 hover:bg-green-700 duration-200 text-white font-semibold py-2 px-4 rounded"
+                className="bg-gray-700 hover:bg-green-700 duration-200 text-white font-semibold py-2 px-4 rounded text-xs md:text-sm"
               >
                 編集
               </button>
